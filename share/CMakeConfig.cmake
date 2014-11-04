ccb9ad1337468f9bc6b3c8d72bc4a05ceef2fa47
Kevin Brightell <kevin.brightwell2@gmail.com>
2014-11-04 17:11:56 -0500

    Fix errors with cmake and pkg-config files, hopefully fix travis build.

--- share/CMakeConfig.cmake
+++ share/CMakeConfig.cmake
@@ -1,31 +1,31 @@
-# - Try to find qhttpserver
+# - Try to find QtWebService
 # Once done this will define
-#  QHTTPSERVER_FOUND - System has qhttpserver
-#  QHTTPSERVER_INCLUDE_DIRS - The qhttpserver include directories
-#  QHTTPSERVER_LIBRARIES - The libraries needed to use qhttpserver
-#  QHTTPSERVER_DEFINITIONS - Compiler switches required for using qhttpserver
+#  QTWEBSERVICE_FOUND - System has qhttpserver
+#  QTWEBSERVICE_INCLUDE_DIRS - The qhttpserver include directories
+#  QTWEBSERVICE_LIBRARIES - The libraries needed to use qhttpserver
+#  QTWEBSERVICE_DEFINITIONS - Compiler switches required for using qhttpserver
 
 find_package(PkgConfig)
 
-pkg_check_modules(PC_QHTTPSERVER QUIET @qhttpserver_LIB_NAME@-@qhttpserver_LIB_VERSION@)
+pkg_check_modules(PC_QTWEBSERVICE QUIET @QtWebService_LIB_NAME@-@QtWebService_LIB_VERSION@)
 
-set(QHTTPSERVER_DEFINITIONS ${PC_QHTTPSERVER_CFLAGS_OTHER})
+set(QTWEBSERVICE_DEFINITIONS ${PC_QTWEBSERVICE_CFLAGS_OTHER})
 
-find_path(QHTTPSERVER_INCLUDE_DIR @qhttpserver_LIB_NAME@/qhttpserver.h
-          HINTS ${PC_QHTTPSERVER_INCLUDEDIR} ${PC_QHTTPSERVER_INCLUDE_DIRS}
-          PATH_SUFFIXES qhttpserver )
+find_path(QTWEBSERVICE_INCLUDE_DIR @QtWebService_LIB_NAME@/QWebService.h
+          HINTS ${PC_QTWEBSERVICE_INCLUDEDIR} ${PC_QTWEBSERVICE_INCLUDE_DIRS}
+          PATH_SUFFIXES QtWebService )
 
-find_library(QHTTPSERVER_LIBRARY NAMES @qhttpserver_LIB_NAME@ lib@qhttpserver_LIB_NAME@
-             HINTS ${PC_QHTTPSERVER_LIBDIR} ${PC_QHTTPSERVER_LIBRARY_DIRS} )
+find_library(QTWEBSERVICE_LIBRARY NAMES @QtWebService_LIB_NAME@ lib@QtWebService_LIB_NAME@
+             HINTS ${PC_QTWEBSERVICE_LIBDIR} ${PC_QTWEBSERVICE_LIBRARY_DIRS} )
 
-set(QHTTPSERVER_LIBRARIES ${QHTTPSERVER_LIBRARY} )
-set(QHTTPSERVER_INCLUDE_DIRS ${QHTTPSERVER_INCLUDE_DIR} )
+set(QTWEBSERVICE_LIBRARIES ${QTWEBSERVICE_LIBRARY} )
+set(QTWEBSERVICE_INCLUDE_DIRS ${QTWEBSERVICE_INCLUDE_DIR} )
 
 include(FindPackageHandleStandardArgs)
 
-# handle the QUIETLY and REQUIRED arguments and set QHTTPSERVER_FOUND to TRUE
+# handle the QUIETLY and REQUIRED arguments and set QTWEBSERVICE_FOUND to TRUE
 # if all listed variables are TRUE
-find_package_handle_standard_args(qhttpserver  DEFAULT_MSG
-                                  QHTTPSERVER_LIBRARY QHTTPSERVER_INCLUDE_DIR)
+find_package_handle_standard_args(QtWebService  DEFAULT_MSG
+                                  QTWEBSERVICE_LIBRARY QTWEBSERVICE_INCLUDE_DIR)
 
-mark_as_advanced(QHTTPSERVER_INCLUDE_DIR QHTTPSERVER_LIBRARY )
+mark_as_advanced(QTWEBSERVICE_INCLUDE_DIR QTWEBSERVICE_LIBRARY )
